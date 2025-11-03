2d29b17d8c50b034525a53d09ccba369740206cf
girish <gshenai@gmail.com>
2025-11-02 22:58:01 -0500

    add support for Limelight to read AprilTag Motif

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/circuitBreaker/utility/Limelight3AAprilTag.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.circuitBreaker.utility;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.List;
+
+public class Limelight3A {
+
+    private com.qualcomm.hardware.limelightvision.Limelight3A limelight;
+}
